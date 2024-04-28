9813d8b01bf6ad3f06f5b06be5b1227ae9993981
tacsquang <168155554+tacsquang@users.noreply.github.com>
2024-04-28 08:52:19 +0700

    Update sched.c
    Thêm khóa mutex để tránh sự bất đồng bộ

--- src/sched.c
+++ src/sched.c
@@ -43,6 +43,8 @@ void init_scheduler(void) {
  *  State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
  */
 struct pcb_t * get_mlq_proc(void) {
+	pthread_mutex_lock(&queue_lock);
+	
 	struct pcb_t * proc = NULL;
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
@@ -67,6 +69,7 @@ struct pcb_t * get_mlq_proc(void) {
 		}
 	}
 	//proc = dequeue(&mlq_ready_queue[0]);
+	pthread_mutex_unlock(&queue_lock);
 	return proc;	
 }
 
@@ -95,6 +98,8 @@ void add_proc(struct pcb_t * proc) {
 }
 #else
 struct pcb_t * get_proc(void) {
+	pthread_mutex_lock(&queue_lock);
+	
 	struct pcb_t * proc = NULL;
 	/*TODO: get a process from [ready_queue].
 	 * Remember to use lock to protect the queue.
@@ -105,6 +110,8 @@ struct pcb_t * get_proc(void) {
 		proc = dequeue(&ready_queue);
 		
 	}
+
+	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
